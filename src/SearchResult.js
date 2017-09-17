8583562aa3d8bc7993a1fd04b65a5c8cffea83aa
Wendy <tell.wendy@gmail.com>
2017-09-16 17:51:02 -0700

    front end structure

---
+++ src/SearchResult.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MapResult from "./MapResult";
+import Results from "./Results";
+
+const SearchResult = () => {
+  return (
+    <div>
+      <MapResult />
+      <Results />
+    </div>
+    );
+}
+
+export default SearchResult;
